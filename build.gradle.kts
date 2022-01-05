d26fe9746e7c335a885d59ed2d34486265e422dc
Joffrey Bion <joffrey.bion@gmail.com>
2022-01-05 18:06:54 +0100

    Upgrade changelog version to 1.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     `maven-publish`
     signing
     id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
-    id("org.hildan.github.changelog") version "1.8.0"
+    id("org.hildan.github.changelog") version "1.9.0"
 }
 
 group = "org.hildan.hashcode"
