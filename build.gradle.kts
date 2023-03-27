55cb1496f366760458aac340c14844155a1a72f1
Joffrey Bion <joffrey.bion@gmail.com>
2023-03-28 01:42:59 +0200

    Upgrade changelog generator to 1.13.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     `maven-publish`
     signing
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
-    id("org.hildan.github.changelog") version "1.13.0"
+    id("org.hildan.github.changelog") version "1.13.1"
 }
 
 group = "org.hildan.hashcode"
